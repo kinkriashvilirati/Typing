46a681db695afa64792ba844aa4a5e8cdb8a2aa8
Rati Kinkriashvili <kinkriashvilirati@gmail.com>
2026-06-29 11:10:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const TEST_TEXTS = [
   {
     title: "Second test",
     text: "I am happy now. I do not wait for things to change. I am happy now and things will change. My state of being comes first. Reality comes second. I am happy now and I watch what happens. I do not wait to be happy. I do not say I will be happy when this or that happens. I know it works the other way. I am happy now. I choose to be happy now. I am happy now and the world follows. I am happy now. This is the secret. I am happy now."
+  },
+  {
+    title: "Third test",
+    text: "Now is all there is. Now is the only moment. The past is a memory I am having now. The future is a dream I am having now. Everything is now. All my power is now. I can only change things now. I can only feel now. I can only act now. So I am present now. I do not live in the past. I do not wait for the future. I am here now. Now is all there is. Now is where life happens. I am here now. I am present now."
   }
 ];
 const TEST_DURATION_SECONDS = 180;
